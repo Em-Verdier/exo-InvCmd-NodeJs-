d513e55534fc81a221400ced06c6b4c2d0286bf3
Em-Verdier <verdierem@gmail.com>
2020-09-24 09:57:05 +0200

    completed

--- src/sayGoodbye.js
+++ src/sayGoodbye.js
@@ -1,7 +1,16 @@
-let string = process.argv[2]
-console.log(`Goodbye, ${string}`)
+/* Ecrivez un script sayGoodbye.js qui prend 
+en argument de la ligne de commande une string 
+et affiche un message comme ci-dessous: */
 
 if (process.argv.length != 3) {
-    console.log("avertissment: sayGoodbye.js ne s'execute pas")
+    console.log('caution: sayGoodbye.js not executed, too much words')
     process.exit(1)
 }
+
+if (!isNaN(process.argv[2])) {
+    console.log(`Error: ${process.argv[2]} not characters`)
+    process.exit(1)
+}
+
+let string = process.argv[2]
+console.log(`Goodbye, ${string}`)
